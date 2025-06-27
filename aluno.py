7a17962d31587812a2b8125e3941f8b4a5f58c2d
Gleydson Silva <gleydsonluidy2@gmail.com>
2025-06-27 18:37:04 -0300

    Atualiza funções do banco de dados

--- aluno.py
+++ aluno.py
@@ -1,36 +1,59 @@
 from database import Database
 
 class Aluno:
-    def __init__(self,nome, idade, curso):
+    def __init__(self,nome, idade, curso_nome):
         self.nome = nome 
         self.idade = idade
-        self.curso = curso
+        self.curso_nome = curso_nome
 
     def salvar_no_banco(self):
         db = Database()
-        sql_verifica = "SELECT * FROM alunos WHERE nome = %s AND curso = %s "
-        resultado= db.consultar(sql_verifica, (self.nome, self.curso))
+        
+        sql_curso = "SELECT id FROM cursos WHERE nome = %s"
+        resultado = db.consultar(sql_curso, (self.curso_nome,))
+
         if resultado:
+            curso_id = resultado[0][0]
+        else:
+            db.executar("INSERT INTO cursos (nome) VALUES (%s)", (self.curso_nome,))
+            curso_id = db.consultar("SELECT LAST_INSERT_ID()")[0][0]
+        
+        sql_verifica = "SELECT * FROM alunos WHERE nome = %s AND curso_id = %s "
+        resultado= db.consultar(sql_verifica, (self.nome, curso_id))
+        if db.consultar(sql_verifica, (self.nome, curso_id)):
             print("Já existe um aluno com esse nome e curso.")
             db.fechar()
             return
         
-        sql = "INSERT INTO alunos (nome, idade, curso)   VALUES (%s, %s, %s)"
-        valores = (self.nome, self.idade, self.curso)
+        sql = "INSERT INTO alunos (nome, idade, curso_id)  VALUES (%s, %s, %s)"
+        valores = (self.nome, self.idade, curso_id)
         db.executar(sql,valores)
         db.fechar()
 
     def atualizar_no_banco(self, id_aluno):
         db = Database()
-        sql = "UPDATE alunos SET nome = %s, idade = %s, curso = %s WHERE id = %s"
-        valores = (self.nome, self.idade, self.curso, id_aluno)
+        sql_curso = "SELECT id FROM cursos WHERE nome = %s"
+        resultado = db.consultar(sql_curso, (self.curso_nome,))
+        if resultado:
+            curso_id = resultado[0][0]
+        else:
+            db.executar("INSERT INTO cursos (nome) VALUES (%s)", (self.curso_nome,))
+            curso_id = db.consultar("SELECT LAST_INSERT_ID()")[0][0]
+        
+        sql = "UPDATE alunos SET nome = %s, idade = %s, curso_id = %s WHERE id = %s"
+        valores = (self.nome, self.idade, curso_id, id_aluno)
         db.executar(sql, valores)
         db.fechar()
 
     @staticmethod
     def buscar_por_id(id_aluno):
         db = Database()
-        sql = "SELECT * FROM alunos WHERE id = %s"
+        sql = """
+            SELECT a.id, a.nome, a.idade, c.nome
+            FROM alunos a
+            JOIN cursos c ON a.curso_id = c.id
+            WHERE a.id = %s
+        """
         db.cursor.execute(sql, (id_aluno,))
         resultado = db.cursor.fetchone()
         db.fechar()
@@ -39,7 +62,11 @@ class Aluno:
     @staticmethod
     def listar_alunos():
         db = Database()
-        sql = "SELECT * FROM alunos"
+        sql = """
+            SELECT a.id, a.nome, a.idade, c.nome
+            FROM alunos a
+            JOIN cursos c ON a.curso_id = c.id
+        """
         resultados = db.consultar(sql)
         db.fechar()
         return resultados
@@ -52,20 +79,28 @@ class Aluno:
         db.fechar()
 
     @staticmethod
-    def buscar_por_nome(parte_nome):
+    def buscar_por_nome(nome):
         db = Database()
-        sql = "SELECT * FROM alunos WHERE nome LIKE %s"
-        valores = (f"%{parte_nome}%",)
-        resultados = db.consultar(sql, valores)
+        sql = """
+            SELECT a.id, a.nome, a.idade, c.nome
+            FROM alunos a
+            JOIN cursos c ON a.curso_id = c.id
+            WHERE a.nome LIKE  %s
+        """
+        resultados = db.consultar(sql, (f"%{nome}%",))
         db.fechar()
         return resultados
     
     @staticmethod
-    def buscar_por_curso(nome_curso):
+    def buscar_por_curso(curso_nome):
         db = Database()
-        sql = ("SELECT * FROM alunos WHERE curso LIKE %s")
-        valores = (f"%{nome_curso}%",)
-        resultados = db.consultar(sql, valores)
+        sql = """"
+             SELECT a.id, a.nome, a.idade, c.nome
+        FROM alunos a
+        JOIN cursos c ON a.curso_id = c.id
+        WHERE c.curso LIKE %s
+        """
+        resultados = db.consultar(sql, (f"%{curso_nome}%",))
         db.fechar()
         return resultados
     
